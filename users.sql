4e3bf81f886a4cabccb722669558471d28f92612
maboglia <mauro.bogliaccino@gmail.com>
2018-10-22 14:34:14 +0200

    upl

---
+++ users.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 13, 2018 at 07:43 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `codexworld`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `status` enum('1','0') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `phone`, `created`, `modified`, `status`) VALUES
+(1, 'mauro bogliaccino', 'mauro@gmail.com', '6666666666', '2018-07-13 08:23:23', '2018-07-13 08:23:23', '1'),
+(2, 'paolo bogliaccino', 'paolo@gmail.com', '8888888888', '2018-07-12 08:23:23', '2018-07-12 08:23:23', '1'),
+(3, 'giuseppe garibaldi', 'giuseppe@gmail.com', '1234321564', '2018-07-14 08:23:23', '2018-07-14 08:23:23', '1'),
+(4, 'john lennon', 'john@gmail.com', '9999999999', '2018-07-10 08:23:23', '2018-07-10 08:23:23', '0'),
+(5, 'giovanna verdi', 'giovanna@gmail.com', '9898899889', '2018-07-09 08:23:23', '2018-07-09 08:23:23', '1'),
+(6, 'raffaella bella', 'raffaella@gmail.com', '7777777777', '2018-06-28 08:23:23', '2018-06-28 08:23:23', '0');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
